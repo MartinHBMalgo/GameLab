92799798b58dd728b6e643bd5cc16400281561b5
Martin, Hansson Böhm <martin.hansson.bohm@edu.sgy.se>
2025-04-10 10:22:04 +0200

    Update movementController finished

--- Assets/MovementController.cs
+++ Assets/MovementController.cs
@@ -26,9 +26,52 @@ public class Movement : MonoBehaviour
         }
     }
 
+    public void Move(float horizontalInput)
+    {
+        targetHorizontalSpeed = horizontalInput * moveSpeed;
+    }
+
+    public void RequestJump()
+    {
+        jumpRequested = true;
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector3(targetHorizontalSpeed, rb.linearVelocity.y, 0);
+        bool isGrounded = Physics.OverlapSphere(groundCheckTransform.position, groundCheckSize, groundLayer).Length > 0;
+        bool isOnBouncy = Physics.OverlapSphere(groundCheckTransform.position, groundCheckSize, bounceLayer).Length > 0;
+
+        // Tillåt bara hopp om vi är på marken
+        // Om vi är på bouncy - Studsa.
+        if (isOnBouncy)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, 0); // Clear vertical velocity
+            rb.AddForce(Vector3.up * bounceForce, ForceMode.VelocityChange);
+        }
+        else if (jumpRequested && isGrounded)
+        {
+            rb.AddForce(new Vector3(0, 5f, 0), ForceMode.VelocityChange);
+            jumpRequested = false;
+        }
+        // If we're in the air and space was pressed, clear the flag
+        else if (!isGrounded && jumpRequested)
+        {
+            jumpRequested = false;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnDrawGizmos() {
+        if (groundCheckTransform == null) return; 
+        // Detta gör vi för att undvika UnassignedReferenceException ifall vi inte länkat en
+        // Transform till groundCheckTransform
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(groundCheckTransform.position, groundCheckSize);
     }
 }
